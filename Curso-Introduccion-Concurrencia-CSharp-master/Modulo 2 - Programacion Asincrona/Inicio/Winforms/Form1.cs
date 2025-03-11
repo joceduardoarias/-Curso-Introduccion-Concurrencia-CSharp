c9494ca57c6f21583af737ba3d31c7eda5251251
Jose Arias <josee.ariasmelo@gmail.com>
2025-03-11 16:21:11 -0300

    Deadlock issue in btnIniciar_Click and update API URL

--- Curso-Introduccion-Concurrencia-CSharp-master/Modulo 2 - Programacion Asincrona/Inicio/Winforms/Form1.cs
+++ Curso-Introduccion-Concurrencia-CSharp-master/Modulo 2 - Programacion Asincrona/Inicio/Winforms/Form1.cs
@@ -16,16 +16,26 @@ namespace Winforms
 {
     public partial class Form1 : Form
     {
+        private string apiURL;
+        private HttpClient client;
         public Form1()
         {
             InitializeComponent();
+            apiURL = "https://localhost:44313";
+            client = new HttpClient();
         }
 
         private async void btnIniciar_Click(object sender, EventArgs e)
         {   
             Console.WriteLine("Iniciando...");
             loadingGIF.Visible = true;
-            await Esperar(); //Cuando se ejecuta el await, el hilo principal se libera y se ejecuta el metodo Esperar en otro hilo.
+            // Aquí ocurre un problema de deadlock
+            // ObtenerValor() es un método asincrónico, pero se está esperando su resultado de manera sincrónica con `.Result`.
+            // En una aplicación de WinForms, el hilo principal maneja la UI y tiene un "contexto de sincronización".
+            // Cuando `ObtenerValor().Result` se ejecuta, el hilo principal queda bloqueado esperando el resultado.
+            // Sin embargo, `ObtenerValor` intenta reanudar su ejecución en el mismo hilo principal cuando `await Esperar();` finaliza.
+            // Como el hilo principal está bloqueado esperando `.Result`, el código nunca se reanuda, generando un *deadlock*.
+            var valor = ObtenerValor().Result; 
             MessageBox.Show("Pasaron los 5 segundos");
             loadingGIF.Visible = false;
             Console.WriteLine("Finalizado!");
@@ -37,6 +47,12 @@ namespace Winforms
         private async Task Esperar()
         {
             await Task.Delay(5000);
+        }
+
+        private async Task<string> ObtenerValor()
+        {
+            await Esperar();
+            return "Hola Mundo"; 
         }
     }
 }
