26ee9b90b72a75b8f63772a3f6a3411d87aba710
Jose Arias <josee.ariasmelo@gmail.com>
2025-03-18 16:39:17 -0300

    Secuencial vs Paralelo
    - Se utiliza Task.WhenAll para hacer paralelismo.

--- Curso-Introduccion-Concurrencia-CSharp-master/Modulo 5 - Programacion en Paralelo/Inicio/Winforms/Form1.cs
+++ Curso-Introduccion-Concurrencia-CSharp-master/Modulo 5 - Programacion en Paralelo/Inicio/Winforms/Form1.cs
@@ -30,10 +30,76 @@ namespace Winforms
 
         private async void btnIniciar_Click(object sender, EventArgs e)
         {
+            var directorioBase = AppDomain.CurrentDomain.BaseDirectory;
+            var destinoBaseParalela = Path.Combine(directorioBase, @"Imagenes\resultado-Paralela");
+            var destinoBaseSecuencial = Path.Combine(directorioBase, @"Imagenes\resultado-Secuencial");
+            PrepararEjecucion(destinoBaseParalela, destinoBaseSecuencial);
+            
+            var images = ObtenerImagenes();
+            
+            //Procesamiento secuencial
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+            foreach (var image in images)
+            {
+                await ProcesarImagenes(image, destinoBaseSecuencial);
+            }
+            
+            Console.WriteLine("Secuencial: duración en segundos: {0}", stopwatch.Elapsed.TotalSeconds);
+            
+            stopwatch.Restart();
+            
+            //Procesamiento paralelo
+            var tasks = images.Select(image => ProcesarImagenes(image, destinoBaseParalela));
+            await Task.WhenAll(tasks);
+            
+            Console.WriteLine("Paralelo: duración en segundos: {0}", stopwatch.Elapsed.TotalSeconds);
+            stopwatch.Stop();
         }
-
         private void btnCancelar_Click(object sender, EventArgs e)
         {
+        }
+        private void PrepararEjecucion(string destinoBaseParalela, string destinoBaseSecuencial)
+        {
+            if (!Directory.Exists(destinoBaseParalela))
+            {
+                Directory.CreateDirectory(destinoBaseParalela);
+            }
+            if (!Directory.Exists(destinoBaseSecuencial))
+            {
+                Directory.CreateDirectory(destinoBaseSecuencial);
+            }
+            BorrarArchivos(destinoBaseParalela, destinoBaseSecuencial);
+        }
+        private void BorrarArchivos(string destinoBaseParalela, string destinoBaseSecuencial)
+        {
+            foreach (var file in Directory.GetFiles(destinoBaseParalela))
+            {
+                File.Delete(file);
+            }
+            foreach (var file in Directory.GetFiles(destinoBaseSecuencial))
+            {
+                File.Delete(file);
+            }
+        }
+        private static List<Imagen> ObtenerImagenes()
+        {
+            var imagenes = new List<Imagen>();
+            imagenes.Add(new Imagen { Nombre = "imagen1.jpg", Url = "https://randomuser.me/api/portraits/men/75.jpg" });
+            imagenes.Add(new Imagen { Nombre = "imagen2.jpg", Url = "https://randomuser.me/api/portraits/men/76.jpg" });
+            imagenes.Add(new Imagen { Nombre = "imagen3.jpg", Url = "https://randomuser.me/api/portraits/men/77.jpg" });
+            imagenes.Add(new Imagen { Nombre = "imagen4.jpg", Url = "https://randomuser.me/api/portraits/men/78.jpg" });
+            imagenes.Add(new Imagen { Nombre = "imagen5.jpg", Url = "https://randomuser.me/api/portraits/men/79.jpg" });
+            return imagenes;
+        }
+        private async Task ProcesarImagenes(Imagen imagen, string directorio)
+        {
+            var response = await httpClient.GetAsync(imagen.Url);
+            var bytes = await response.Content.ReadAsByteArrayAsync();
+            Bitmap bitmap = new Bitmap(Image.FromStream(new MemoryStream(bytes)));
+            bitmap.RotateFlip(RotateFlipType.Rotate180FlipNone);
+            var destino = Path.Combine(directorio, imagen.Nombre);
+            bitmap.Save(destino);
         }
     }
 }
